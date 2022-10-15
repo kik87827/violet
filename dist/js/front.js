dea832f6015652985529c3cf4235dcd102612a08
kiminkyung <kik87827@gmail.com>
2022-10-15 11:03:01 +0900

    진행중 1

---
+++ dist/js/front.js
@@ -0,0 +1,266 @@
+document.addEventListener("DOMContentLoaded", function() {
+  commonInit();
+  commonEvent();
+});
+window.addEventListener("load", function() {});
+
+function commonInit() {
+  let touchstart = "ontouchstart" in window;
+  let userAgent = navigator.userAgent.toLowerCase();
+  if (touchstart) {
+    browserAdd("touchmode");
+  }
+  if (userAgent.indexOf('samsung') > -1) {
+    browserAdd("samsung");
+  }
+
+  if (navigator.platform.indexOf('Win') > -1 || navigator.platform.indexOf('win') > -1) {
+    browserAdd("window");
+  }
+
+  if (userAgent.match(/iPad/i) || userAgent.match(/iPhone/i)) {
+    // iPad or iPhone
+    browserAdd("ios");
+  }
+
+
+  function browserAdd(opt) {
+    document.querySelector("html").classList.add(opt);
+  }
+}
+
+function commonEvent() {
+  let windowWidth = 0;
+  action();
+  window.addEventListener("resize", () => {
+    if (windowWidth === window.innerWidth) {
+      return;
+    }
+    action();
+    windowWidth = window.innerWidth;
+  });
+
+  function action() {
+    function commonTitle() {
+      const header_wrap = document.querySelector(".header_wrap");
+      const headerTitle = document.querySelector(".header_title");
+      if (header_wrap === null) {
+        return
+      }
+      const headerElements = [].slice.call(header_wrap.children);
+      let headerElementsWid = [];
+      let headerElementsWidMax = 0;
+      headerElements.forEach((element) => {
+        if (element.classList.contains("header_title")) {
+          return;
+        }
+        headerElementsWid.push(element.getBoundingClientRect().width)
+      });
+      headerElementsWidMax = Math.max.apply(null, headerElementsWid);
+      // let btn_total_wid = btn_total !== null ? btn_total.getBoundingClientRect().width : 0;
+      // const header_util_wrap = document.querySelector(".header_util_wrap");
+      // let header_util_wrap_wid = header_util_wrap !== null ? header_util_wrap.getBoundingClientRect().width : 0;
+      if (headerTitle !== null) {
+        headerTitle.style.paddingLeft = headerElementsWidMax + "px";
+        headerTitle.style.paddingRight = headerElementsWidMax + "px";
+      }
+    }
+    commonTitle();
+  }
+}
+
+
+
+
+function DesignModal(option) {
+  this.message = option.message;
+  this.domHtml = document.querySelector("html");
+  this.domBody = document.querySelector("body");
+  this.pagewrap = document.querySelector(".page_wrap");
+  this.design_modal_wrap = null;
+  this.btn_dmsmidentify = null;
+  this.btn_dmsmcancel = null;
+  this.duration = option.duration !== undefined ? option.duration : 400;
+
+  this.initShow(option);
+}
+
+DesignModal.prototype.initShow = function(option) {
+  var innerPublish = '';
+  var objThis = this;
+
+  // innerPublish += "<div class='design_modal_wrap'>";
+  // innerPublish += "  <div class='bg_design_modal'></div>";
+  // innerPublish += "  <div class='design_modal_w'>";
+  // innerPublish += "          <div class='design_modal'>";
+  // innerPublish += "              <div class='design_modal_cont_w'><div class='design_modal_text'></div></div>";
+  // innerPublish += "              <div class='btn_dmsm_wrap'>";
+  // innerPublish += "                  <a href='javascript:;' class='btn_dmsm close_dmtrigger btn_dmsmidentify'>확인</a>";
+  // if (option.type === "confirm") {
+  //   innerPublish += "              <a href='javascript:;' class='btn_dmsm close_dmtrigger btn_dmsmcancel'>취소</a>";
+  // }
+  // innerPublish += "              </div>";
+  // innerPublish += "          </div>";
+  // innerPublish += "  </div>";
+  // innerPublish += "</div>";
+
+
+
+  this.modalparent = document.createElement('div');
+  this.pagewrap.appendChild(this.modalparent);
+  this.modalparent.classList.add("design_modal_insert_wrap");
+  let btnCloseText = option.btnCloseText !== undefined ? option.btnCloseText : '닫기';
+  let btnMainText = option.btnMainText !== undefined ? option.btnMainText : '확인';
+  this.modalparent.innerHTML = `
+    <div class='design_modal_wrap'>
+        <div class='bg_design_modal'></div>
+        <div class='design_modal_w'>
+            <div class='design_modal'>
+                <div class='design_modal_cont_w'>
+                    <div class='design_modal_text'></div>
+                </div>
+                <div class='btn_dmsm_wrap'>
+                    <a href='javascript:;' class='btn_dmsm close_dmtrigger'>${btnCloseText}</a>
+                    ${option.type === "confirm" ? `<a href='javascript:;' class='btn_dmsm close_dmtrigger btn_main'>${btnMainText}</a>` : ``}
+                </div>
+            </div>
+        </div>
+    </div>`;
+
+  if (option.type === "confirm" || option.type === "alert") {
+    this.design_modal_text = document.querySelector(".design_modal_text");
+    this.design_modal_text.innerHTML = option.message;
+  }
+  if (option.type === "confirm") {}
+  this.btn_main = this.modalparent.querySelector(".btn_main");
+  this.pagewrap.style.zIndex = 0;
+  this.domBody.setAttribute("data-scr", window.pageYOffset);
+  this.domBody.style.marginTop = -window.pageYOffset + "px";
+  this.domHtml.classList.add("touchDis");
+  this.design_modal_wrap = document.querySelector(".design_modal_wrap");
+  this.closetrigger = document.querySelectorAll(".close_dmtrigger");
+  this.design_modal_wrap.classList.add("active");
+  setTimeout(function() {
+    objThis.design_modal_wrap.classList.add("motion");
+  }, 30);
+  this.bindEvent(option);
+}
+DesignModal.prototype.removeHide = function(option) {
+  var objThis = this;
+  this.design_modal_wrap.classList.remove("motion");
+  setTimeout(() => {
+    objThis.design_modal_wrap.classList.remove("active");
+    document.querySelector(".design_modal_insert_wrap").remove();
+    objThis.design_modal_wrap.remove();
+    objThis.domHtml.classList.remove("touchDis");
+    objThis.domBody.style.marginTop = 0;
+
+    window.scrollTo(0, Number(objThis.domBody.getAttribute("data-scr")));
+
+    if (option !== undefined) {
+      if ("closeCallback" in option) {
+        console.log(option)
+        option.closeCallback();
+      }
+    }
+  }, 530);
+}
+DesignModal.prototype.bindEvent = function(option) {
+  var objThis = this;
+  for (var i = 0; i < this.closetrigger.length; i++) {
+    this.closetrigger[i].addEventListener("click", function() {
+      objThis.removeHide(option);
+    }, false);
+  }
+
+
+
+  if (this.btn_main !== null) {
+    this.btn_main.addEventListener("click", function() {
+      setTimeout(() => {
+        if ("btnMainCallback" in option) {
+          option.btnMainCallback();
+        }
+      }, 530);
+    }, false);
+  }
+}
+
+
+
+
+function DesignPopup(option) {
+  this.selector = null;
+  if (option.selector !== undefined) {
+    this.selector = document.querySelector(option.selector);
+  }
+  this.design_popup_wrap = document.querySelectorAll(".popup_wrap");
+  this.domHtml = document.querySelector("html");
+  this.domBody = document.querySelector("body");
+  this.pagewrap = document.querySelector(".page_wrap");
+  this.btn_closeTrigger = null;
+  this.btn_close = null;
+  this.bg_design_popup = null;
+  this.scrollValue = 0;
+  this.popupShow(option.selector);
+}
+
+DesignPopup.prototype.popupShow = function(target) {
+  var objThis = this;
+  this.selector = document.querySelector(target);
+  if (this.selector == null) {
+    return;
+  }
+  this.domBody.setAttribute("data-scr", window.pageYOffset);
+  this.domBody.style.marginTop = -window.pageYOffset + "px";
+  this.scrollValue = window.pageYOffset;
+  this.domHtml.classList.add("touchDis");
+  this.selector.classList.add("active");
+  setTimeout(function() {
+    objThis.selector.classList.add("motion");
+  }, 30);
+
+
+  this.btn_closeTrigger = this.selector.querySelectorAll(".close_trigger");
+  this.btn_close = this.selector.querySelectorAll(".popup_close");
+  this.bg_design_popup = this.selector.querySelector(".popup_wrap .bg_dim");
+  this.domBody.append(this.selector);
+  this.bindEvent(this.selector);
+
+}
+DesignPopup.prototype.popupHide = function(target) {
+  var objThis = this;
+  if (target !== undefined) {
+    if (typeof target == "object") {
+      this.selector = target;
+    } else {
+      this.selector = document.querySelector(target);
+    }
+    this.selector.classList.remove("motion");
+    setTimeout(function() {
+      //remove
+      objThis.selector.classList.remove("active");
+      objThis.design_popup_wrap_active = document.querySelectorAll(".popup_wrap.active");
+      if (objThis.design_popup_wrap_active.length == 0) {
+        objThis.domHtml.classList.remove("touchDis");
+        objThis.domBody.style.marginTop = 0;
+        window.scrollTo(0, parseInt(objThis.domBody.getAttribute("data-scr")));
+      }
+    }, 420);
+  }
+}
+
+DesignPopup.prototype.bindEvent = function() {
+  var objThis = this;
+  var closeItemArray = [...this.btn_closeTrigger, ...this.btn_close];
+  if (this.bg_design_popup !== null) {
+    closeItemArray.push(this.bg_design_popup);
+  }
+  if (closeItemArray.length) {
+    closeItemArray.forEach((element) => {
+      element.addEventListener("click", () => {
+        this.popupHide(objThis.selector);
+      }, false);
+    });
+  }
+};
